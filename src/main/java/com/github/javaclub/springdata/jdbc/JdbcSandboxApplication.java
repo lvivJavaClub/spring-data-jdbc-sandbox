c707da2a560b199476e424bb58f491bc6ddb3e64
ishevtsov <java.shevtsov@gmail.com>
2021-12-14 18:32:57 +0000

    initial commit

---
+++ src/main/java/com/github/javaclub/springdata/jdbc/JdbcSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.github.javaclub.springdata.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSandboxApplication.class, args);
+	}
+
+}
